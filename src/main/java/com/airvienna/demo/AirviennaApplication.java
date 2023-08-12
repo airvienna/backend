df3026eabbfa62c80a674f8dda3d4d593776ca92
seongjun-noh <seongjun5657@gmail.com>
2023-08-12 21:34:16 +0900

    프로젝트 생성

---
+++ src/main/java/com/airvienna/demo/AirviennaApplication.java
@@ -0,0 +1,13 @@
+package com.airvienna.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirviennaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirviennaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airvienna/demo/AirviennaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airvienna.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirviennaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
