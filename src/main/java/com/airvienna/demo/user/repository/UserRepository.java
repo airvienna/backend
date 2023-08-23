dcef054d65fd9f930b7835f037848eba818892de
seongjun-noh <seongjun5657@gmail.com>
2023-08-24 03:29:26 +0900

    feat: 회원가입 기능 구현

--- src/main/java/com/airvienna/demo/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.airvienna.demo.user.repository;
-
-import com.airvienna.demo.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
