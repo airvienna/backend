15b9132907b57b1aca4356f9e12a5bba578ebc35
seongjun-noh <seongjun5657@gmail.com>
2023-09-10 06:42:04 +0900

    feat: H2-Console을 사용하기 위한 설정

--- src/main/java/com/airvienna/demo/security/config/SecurityConfig.java
+++ src/main/java/com/airvienna/demo/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.airvienna.demo.security.jwt.JwtAccessDeniedHandler;
 import com.airvienna.demo.security.jwt.JwtAuthenticationEntryPoint;
 import com.airvienna.demo.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +12,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,7 +25,7 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-
+    
     private final JwtTokenProvider jwtTokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final HandlerMappingIntrospector introspector;
@@ -42,19 +45,32 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable) // CSRF(크로스 사이트 요청 위조) 보안을 비활성화합니다. 주로 REST API에서 사용됩니다.
-                .sessionManagement((sessionManagement) -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // JWT 토큰으로 인증하기 때문에 세션을 생성하지 않습니다.
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(new MvcRequestMatcher(introspector, "/api/auth/**")).permitAll() // 이 주소는 인증 없이 접근 가능합니다.
-                        .anyRequest().authenticated())// 나머지 주소는 인증이 필요합니다.
-                .exceptionHandling((exceptionHandling) -> exceptionHandling
-                        .accessDeniedHandler(jwtAccessDeniedHandler) // 접근 거부시 사용할 핸들러입니다.
-                        .authenticationEntryPoint(jwtAuthenticationEntryPoint) // 인증 실패시 시작되는 포인트입니다.
-                );
+        // CSRF(크로스 사이트 요청 위조) 보안을 비활성화합니다. 주로 REST API에서 사용됩니다.
+        http.csrf(AbstractHttpConfigurer::disable);
 
-        // JWT 보안 설정을 적용합니다.
+        // JWT 토큰으로 인증하기 때문에 세션을 생성하지 않습니다.
+        http.sessionManagement((sessionManagement) -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.authorizeHttpRequests((authorize) -> authorize
+                // H2 Console에 대한 접근 허용
+                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                // 이 주소는 인증 없이 접근 가능
+                .requestMatchers(new MvcRequestMatcher(introspector, "/api/auth/**")).permitAll()
+                // 나머지 주소는 인증이 필요
+                .anyRequest().authenticated());
+
+        http.exceptionHandling((exceptionHandling) -> exceptionHandling
+                // 접근 거부시 사용할 핸들러
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                // 인증 실패시 시작되는 포인트
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+        );
+
+        // h2-console 사용을 위해 SAME ORIGIN에 대하여 iframe 허용
+        http.headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin));
+
+        // JWT 보안 설정을 적용
         http.apply(new JwtSecurityConfig(jwtTokenProvider));
         return http.build();
     }
